f02b1195011e49b646a7afd3cd87d7af264b8bbd
davinci96 <davis@gmail.com>
2020-09-15 18:40:45 -0500

    first commit

---
+++ oneintro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; //libreria
+
+namespace oneintro //
+{
+    class Program //el archivo donde estoy codificando alguna funcionalidad
+    {
+        static void Main(string[] args) // un metodo
+        {
+            Console.WriteLine("Hello World!"); // instrucciones
+            Console.WriteLine("numero uno");
+            int numUno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El numero es: " +numUno); 
+        }
+    }
+}
